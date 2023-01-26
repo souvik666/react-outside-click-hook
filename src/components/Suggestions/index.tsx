2e488898db3b43e263413a4d947d4a368f1a3b0a
souvik666 <peacedynasty0@gmail.com>
2023-01-26 13:08:03 +0530

    fix(*)

--- src/components/Suggestions/index.tsx
+++ src/components/Suggestions/index.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from 'react';
-import useTriggeredSuggestion from '../../hooks/useTriggeredSuggestion';
- 
+import React, { useMemo } from "react";
+import useTriggeredSuggestion from "../../hooks/useTriggeredSuggestion";
+
 interface Props {
   suggestions: string[];
   onSelect: (suggestion: string) => void;
@@ -9,9 +9,15 @@ interface Props {
   inputStyles?: React.CSSProperties;
 }
 
-const AutoSuggestionInput: React.FC<Props> = ({ suggestions, onSelect, triggerCharacter = '@', divStyles, inputStyles }) => {
+const AutoSuggestionInput: React.FC<Props> = ({
+  suggestions,
+  onSelect,
+  triggerCharacter = "@",
+  divStyles,
+  inputStyles,
+}) => {
   const [inputRef, divRef, showDiv, position] = useTriggeredSuggestion();
-  const styles:React.CSSProperties = useMemo(() => ({
+  const styles: React.CSSProperties = {
     position: "fixed",
     top: position.y,
     left: position.x,
@@ -20,12 +26,12 @@ const AutoSuggestionInput: React.FC<Props> = ({ suggestions, onSelect, triggerCh
     boxShadow: "0px 0px 8px 0px rgba(0,0,0,0.2)",
     zIndex: "1",
     width: "150px",
-    ...divStyles
-  }), [position,divStyles])
-  
+    ...divStyles,
+  };
+
   return (
     <div>
-      <input ref={inputRef} id="my-input" style={inputStyles}/>
+      <input ref={inputRef} id="my-input" style={inputStyles} />
       {showDiv && (
         <div ref={divRef} style={styles}>
           {suggestions.map((suggestion, index) => (
